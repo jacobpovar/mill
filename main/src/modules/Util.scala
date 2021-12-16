dd173e19ad5a8c0b92083981748d3462c260d00f
Yakov Povar <y.povar@tinkoff.ru>
2021-12-16 14:24:27 +0300

    Fixup

--- main/src/modules/Util.scala
+++ main/src/modules/Util.scala
@@ -77,11 +77,11 @@ object Util {
               coursier.Module(coursier.Organization("org.apache.logging.log4j"), coursier.ModuleName("log4j-core")),
               "2.16.10"
             ),
-              Dependency(
+            Dependency(
               coursier.Module(coursier.Organization("com/lihaoyi"), coursier.ModuleName(artifact + artifactSuffix)),
               "0.9.10",
               Configuration.empty,
-              Set((coursier.Organization("org.apache.logging.log4j"), coursier.ModuleName("log4j-core")))
+              Set((coursier.Organization("org.apache.logging.log4j"), coursier.ModuleName("log4j-core"))),
               Publication("", Type.empty, Extension.empty, Classifier.empty),
               optional = false,
               transitive = true
